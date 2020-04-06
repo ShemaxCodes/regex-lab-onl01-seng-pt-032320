11c9d04e908c270300ca23a3839d1cad54fc84ef
Jashema Panapa <jcpanapa@gmail.com>
2020-04-06 00:45:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+return true if word.scan(/\w+aeiou/)
+return false if word.scan(/\w[^AEIOUaeiou][^ ]\w/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\b[unUn][a-z]*\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/\b[A-Z]/)
+  return true
+else 
+  false
 end
-
+end
 def valid_phone_number?(phone)
-
+phone.scan
 end
